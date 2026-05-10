c84f9d2ab2c024176c42cac3f712242cd6864ac5
Joelmk28 <joelmuhindok@gmail.com>
2026-05-10 22:14:04 +0100

    init ApiGateWay

---
+++ apigatewapi/src/main/java/com/levraijmk/apigatewapi/ApigatewapiApplication.java
@@ -0,0 +1,13 @@
+package com.levraijmk.apigatewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewapiApplication.class, args);
+    }
+
+}
---
+++ apigatewapi/src/test/java/com/levraijmk/apigatewapi/ApigatewapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levraijmk.apigatewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
